634054101ea1db3a92d6aaf0499798131ed727c8
Lucas Nogueira <lucas_snogueira@hotmail.com>
2019-09-18 01:04:38 -0300

    initial commit

---
+++ app/src/main/java/com/example/nac1_lucasnogueira_3sir_78436/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nac1_lucasnogueira_3sir_78436
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
